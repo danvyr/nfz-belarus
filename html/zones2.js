7b11239070fe9bfd076eda6b2e9041da4f32aaf2
Vitali Danilovich <danvyr@gmail.com>
2020-02-26 13:00:51 +0300

    add new zones

--- html/zones2.js
+++ html/zones2.js
@@ -6,12 +6,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 
 var pointsTextArea = document.getElementById('pointsTextArea');
+// в запретную зону [1, 2, 3, 4, 5, 6, 7,118, 164]
+// в опасную зоны 43, 41(оставить и в запретных для дрона), 49 [41,43, 49]
 
 for (var i = 0; i < circles.length; ++i )
 {
     var circle = circles[i];
     // console.log(zone.name);
-
     // console.log(zone.coordinates[0][0]);
     L.circle([circle.coordinates.lat, circle.coordinates.lng], 
         circle.radius*1000, {
@@ -19,22 +20,54 @@ for (var i = 0; i < circles.length; ++i )
             fillColor: '#FF00FF',
         fillOpacity: 0.5
         }).bindPopup('Номер зоны=' + circle.id).addTo(mymap);
-    // L.marker([circle.coordinates.lat, circle.coordinates.lng])
-    // .addTo(mymap)
-    // .bindPopup(circle.id);
 }
-
+for (var i = 0; i < circlesDanger.length; ++i )
+{
+    var circle = circlesDanger[i];
+    L.circle([circle.coordinates.lat, circle.coordinates.lng], 
+        circle.radius*1000, {
+            color: 'gold',
+            fillColor: '#FFD700',
+        fillOpacity: 0.5
+        }).bindPopup('Номер зоны=' + circle.id).addTo(mymap);
+}
+for (var i = 0; i < circlesForbidden.length; ++i )
+{
+    var circle = circlesForbidden[i];
+    L.circle([circle.coordinates.lat, circle.coordinates.lng], 
+        circle.radius*1000, {
+            color: 'red',
+            fillColor: '#FF0000',
+        fillOpacity: 0.5
+        }).bindPopup('Номер зоны=' + circle.id).addTo(mymap);
+}
 
 for (var i = 0; i < polys.length; ++i )
 {   
     var poly = polys[i];
-
     L.polygon(poly.coordinates, {
         color: 'FUCHSIA',
         fillColor: '#FF00FF',
         fillOpacity: 0.5
     }).bindPopup('Номер зоны=' + poly.id).addTo(mymap);
-
+}
+for (var i = 0; i < polysDanger.length; ++i )
+{   
+    var poly = polysDanger[i];
+    L.polygon(poly.coordinates, {
+        color: 'gold',
+        fillColor: '#FFD700',
+        fillOpacity: 0.5
+    }).bindPopup('Номер зоны=' + poly.id).addTo(mymap);
+}
+for (var i = 0; i < polysForbidden.length; ++i )
+{   
+    var poly = polysForbidden[i];
+    L.polygon(poly.coordinates, {
+        color: 'red',
+        fillColor: '#FF0000',
+        fillOpacity: 0.5
+    }).bindPopup('Номер зоны=' + poly.id).addTo(mymap);
 }
 
 L.geoJSON(border, {
@@ -42,23 +75,8 @@ L.geoJSON(border, {
 }).addTo(mymap);
 
 
-// navigator.geolocation.getCurrentPosition(function(location) {
-//     var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-  
-//     var mymap = L.map('mapid').setView(latlng, 13)
-//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
-//       maxZoom: 18,
-//       id: 'mapbox.streets',
-//       accessToken: 'pk.eyJ1IjoiYmJyb29rMTU0IiwiYSI6ImNpcXN3dnJrdDAwMGNmd250bjhvZXpnbWsifQ.Nf9Zkfchos577IanoKMoYQ'
-//     }).addTo(mymap);
-  
-//     var marker = L.marker(latlng).addTo(mymap);
-//   });
 
-//   L.easyButton('<i class="material-icons" style="font-size:18px;">gps_fixed</i>', function(){
-//     locateMe();
-// }, {position: 'bottomright'}).addTo(mymap);
+
 
 mymap.locate({setView: true, maxZoom: 16});
 function onLocationFound(e) {
